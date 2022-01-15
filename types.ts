144db0ad93352a1f02b5fc8274471d0d456d53f6
Garrett Weems <gwgraphicdesign@gmail.com>
2022-01-15 04:04:59 -0600

    Added pup ts interface

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface Pup {
+  id: number;
+  created_at: string;
+  name: string;
+  birthday: string;
+  colors?: any;
+  mom: number;
+  dad: number;
+  breed: number;
+  avatar?: any;
+  price?: any;
+  available: boolean;
+  embark?: any;
+  sold: boolean;
+}
